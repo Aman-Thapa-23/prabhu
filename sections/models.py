ff46cb1648b8f1188ba4b3bccb442eb3c97c9ea2
Amanthapa <aurluvthapa733@gmail.com>
2021-03-18 13:27:00 +0545

    added

---
+++ sections/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    name= models.CharField(max_length=100, null=True, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=13, null=True)
+    email = models.CharField(max_length=100, null= True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
