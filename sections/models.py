b5fd77e5b3a475501c99622aa6d733287fda2905
Amanthapa <aurluvthapa733@gmail.com>
2021-03-18 13:44:24 +0545

    Prabhu is added

--- sections/models.py
+++ sections/models.py
@@ -7,4 +7,5 @@ class Customer(models.Model):
     contact = models.CharField(max_length=13, null=True)
     email = models.CharField(max_length=100, null= True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    address = models.CharField(max_length=200, null=True)
     
